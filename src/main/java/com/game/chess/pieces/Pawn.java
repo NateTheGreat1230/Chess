797eef8aa5690f6100b58912de7847f32bc13805
NateTheGreat <nathanprattwashburn@gmail.com>
2024-01-20 14:56:02 -0700

    fixed pawn

--- src/main/java/com/game/chess/pieces/Pawn.java
+++ src/main/java/com/game/chess/pieces/Pawn.java
@@ -3,7 +3,6 @@ package com.game.chess.pieces;
 import com.game.chess.Board;
 import com.game.chess.Piece;
 import com.game.chess.Position;
-
 import java.util.ArrayList;
 
 public class Pawn extends Piece {
@@ -20,22 +19,31 @@ public class Pawn extends Piece {
     public ArrayList<Position> getValidMoves(Position current, Board board) {
         ArrayList<Position> possible = new ArrayList<>();
         int direction = (pieceTeam == Team.WHITE) ? -1 : 1;
-        possible.add(new Position(current.getRow() + direction, current.getColumn()));
-        if (current.getRow() == ((pieceTeam == Team.WHITE) ? 6 : 1)) {
-            possible.add(new Position(current.getRow() + 2 * direction, current.getColumn()));
-        }
-        possible.add(new Position(current.getRow() + direction, current.getColumn() + 1));
-        possible.add(new Position(current.getRow() + direction, current.getColumn() - 1));
-        possible.removeIf(position -> position.getRow() < 0 || position.getRow() > 7 || position.getColumn() < 0 || position.getColumn() > 7);
-        ArrayList<Position> valid = new ArrayList<>();
-        for (Position position : possible) {
-            Piece piece = board.getPiece(position);
-            if (piece.pieceTeam.equals(Team.BLANK)) {
-                valid.add(position);
-            } else if (!piece.pieceTeam.equals(this.pieceTeam)) {
-                valid.add(position);
+        Position forwardOne = new Position(current.getRow() + direction, current.getColumn());
+        Position forwardTwo = new Position(current.getRow() + 2 * direction, current.getColumn());
+        Position leftAttack = new Position(current.getRow() + direction, current.getColumn() - 1);
+        Position rightAttack = new Position(current.getRow() + direction, current.getColumn() + 1);
+        if (isValidMove(forwardOne, board)) {
+            possible.add(forwardOne);
+            if (current.getRow() == ((pieceTeam == Team.WHITE) ? 6 : 1) && isValidMove(forwardTwo, board)) {
+                possible.add(forwardTwo);
             }
         }
-        return valid;
+        if (isValidAttack(leftAttack, board)) {
+            possible.add(leftAttack);
+        }
+        if (isValidAttack(rightAttack, board)) {
+            possible.add(rightAttack);
+        }
+        return possible;
+    }
+    private boolean isValidMove(Position position, Board board) {
+        return position.getRow() >= 0 && position.getRow() <= 7 && position.getColumn() >= 0 &&
+               position.getColumn() <= 7 && board.getPiece(position).pieceTeam.equals(Team.BLANK);
+    }
+    private boolean isValidAttack(Position position, Board board) {
+        return position.getRow() >= 0 && position.getRow() <= 7 && position.getColumn() >= 0 &&
+               position.getColumn() <= 7 && !board.getPiece(position).pieceTeam.equals(Team.BLANK) &&
+               !board.getPiece(position).pieceTeam.equals(this.pieceTeam);
     }
 }
